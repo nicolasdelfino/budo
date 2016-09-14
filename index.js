2b3b93c76e57c8e87b24a3b0b862b82a20ae1a4f
Nicolas Delfino <nicolas.delfino@avaloninnovation.com>
2016-09-14 22:33:43 +0200

    Transform to plain Budo lock

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-var fetch = require('./fetch.js')
-
-var promise = fetch('file.txt')
-
-promise.then((names) => {
-   console.log(names)
-})
-
-promise.catch((error) => {
-   console.log('oh oh', error)
-}) 
+console.log('=> budo!');
